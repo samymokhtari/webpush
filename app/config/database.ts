386b83bf14cf28ca3c66157aef9313f500156642
Samy Mokhtari <samy.mokhtari213@gmail.com>
2022-09-19 14:45:44 +0200

    connect mongo to server / fix dockerfile

--- app/config/database.ts
+++ app/config/database.ts
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default async () => {
   // Connect to the database
   try {
-    await mongoose.connect('mongodb://localhost/web-push-notifications', {
+    await mongoose.connect("mongodb://streamzer.fr/web-push-notifications", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    mongoose.set('useCreateIndex', true);
+    mongoose.set("useCreateIndex", true);
   } catch (e) {
     console.error(`Couldn't connect to the database: ${e}`);
     process.exit(1);
